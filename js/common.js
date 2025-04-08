76a5c7ba32b1081e3b148ec4c50ed8e59fee366b
Dat <48687628+1tenxien3chim@users.noreply.github.com>
2025-04-09 00:27:40 +0700

    update image block
    update image block

--- js/common.js
+++ js/common.js
@@ -256,3 +256,30 @@ $(function () {
     });  
 });
 
+$(function () {
+    $(".slider-facilities").owlCarousel({
+        items: 1,
+        responsive: { 
+            991: { items: 1,},
+            768: { items: 1, },
+            480: { items: 1,},
+            0: { items: 1,}
+        },
+        rewind: false,
+        autoplay: false,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1000, //slide speed smooth
+        dots: true,
+        dotsEach: false,
+        loop: true,
+        nav: false,
+        navText: ['<i class="fa fa-angle-left arrow-slider"></i>', '<i class="fa fa-angle-right arrow-slider"></i>'],
+        
+        //animateOut: ['fadeOutUp', 'zoomOut', 'fadeOutLeft'], // default: false
+        //animateIn: ['fadeInDown', 'zoomIn', 'fadeInLeft'], // default: false
+        center: false,
+    });  
+});
+
+
