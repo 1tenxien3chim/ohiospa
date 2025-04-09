82f7bf773236d8ceddcf531351ff29e0605ca6a2
Dat <48687628+1tenxien3chim@users.noreply.github.com>
2025-04-09 21:31:32 +0700

    update thư viện
    update thư viện

--- js/common.js
+++ js/common.js
@@ -282,4 +282,29 @@ $(function () {
     });  
 });
 
+$(function () {
+    $(".img-videos").owlCarousel({
+        items: 1,
+        responsive: { 
+            991: { items: 1,},
+            768: { items: 1, },
+            480: { items: 1,},
+            0: { items: 1,}
+        },
+        rewind: false,
+        autoplay: false,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1000, //slide speed smooth
+        dots: true,
+        dotsEach: false,
+        loop: true,
+        nav: false,
+        navText: ['<i class="fa fa-angle-left arrow-slider"></i>', '<i class="fa fa-angle-right arrow-slider"></i>'],
+        
+        //animateOut: ['fadeOutUp', 'zoomOut', 'fadeOutLeft'], // default: false
+        //animateIn: ['fadeInDown', 'zoomIn', 'fadeInLeft'], // default: false
+        center: false,
+    });  
+});
 
